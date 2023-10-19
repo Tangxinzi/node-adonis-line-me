5c7d413fddb76b5373a05e4f6c428596b554b54e
liaojiangkun@kanzhun.com <liaojiangkun@kanzhun.com>
2023-10-19 08:11:40 +0800

    Create Jwt.ts

---
+++ app/Models/Jwt.ts
@@ -0,0 +1,44 @@
+'use strict'
+
+import Fs from 'fs';
+import Jwtoken from 'jsonwebtoken';
+import Env from '@ioc:Adonis/Core/Env';
+import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Jwt extends BaseModel {
+  // HS256
+  static sign (data) {
+    return Jwtoken.sign(JSON.parse(data), Env.get('APP_KEY'))
+  }
+
+  static verify (token) {
+    return Jwtoken.verify(token, Env.get('APP_KEY'))
+  }
+
+  static decode (token) {
+    return Jwtoken.decode(token, Env.get('APP_KEY'))
+  }
+
+  // RS256
+  static signPrivateKey (data) {
+    try {
+      return Jwtoken.sign(JSON.parse(data), Fs.readFileSync('./config/pem/private.key'), { algorithm: 'RS256' })
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  static verifyPublicKey (token) {
+    try {
+      return Jwtoken.verify(token, Fs.readFileSync('./config/pem/public.key'))
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  static decodeKey (token) {
+    return Jwtoken.decode(token)
+  }
+}
+
+module.exports = Jwt
