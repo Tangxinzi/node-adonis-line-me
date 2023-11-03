3df9f9920fdec360d8a6dac668bab1267c44735e
liaojiangkun@kanzhun.com <liaojiangkun@kanzhun.com>
2023-11-03 18:29:32 +0800

    聊天代码 #1

--- start/socket.ts
+++ start/socket.ts
@@ -1,5 +1,6 @@
 import Database from '@ioc:Adonis/Lucid/Database'
 import Moment from'moment';
+Moment.locale('zh-cn')
 import Jwt from 'App/Models/Jwt';
 import Ws from 'App/Services/Ws'
 Ws.boot()
@@ -7,16 +8,33 @@ Ws.boot()
 /**
  * Listen for incoming socket connections
  */
-Ws.io.on('connection', (socket) => {
-  // socket.emit('news', { hello: 'hello world' })
-
+Ws.io.on('connection', async (socket) => {
   try {
+    const { sign, chat_id } = socket.handshake.headers
+    const user = await Database.from('users').where('id', Jwt.verifyPublicKey(sign || '')).first()
+
     socket.on('send message', async (data) => {
-      if (data.sign) {
-        const user = await Database.from('users').where('id', Jwt.verifyPublicKey(data.sign || '')).first()
-        await Database.table('chats').insert({ chat_id: 'test', user_id: user.user_id, chat_content: data.message, chat_content_type: data.type })
+      if (sign) {
+        await Database.table('chats').insert({ chat_id: data.chat_id, user_id: user.user_id, chat_content: data.message, chat_content_type: data.type })
+
+        const chats = await Database.from('chats').select('id', 'chat_id', 'user_id', 'chat_content', 'chat_content_type').where({ chat_id, status: 1 }).orderBy('created_at', 'asc')
+        socket.broadcast.emit('messages', chats)
+        socket.emit('messages', chats)
       }
     })
+
+    const chats = await Database.from('chats').select('id', 'chat_id', 'user_id', 'chat_content', 'chat_content_type').where({ chat_id, status: 1 }).orderBy('created_at', 'asc')
+    socket.emit('chat messages', chats)
+
+    // 消息列表
+    const chatroom = await Database.from('chatroom').where({ chat_users_id: user.user_id, status: 1 }).orderBy('created_at', 'asc')
+    for (let index = 0; index < chatroom.length; index++) {
+      chatroom[index].message = await Database.from('chats').select('chat_content').where({ chat_id: chatroom[index].chat_id, status: 1 }).orderBy('created_at', 'desc').first()
+      chatroom[index].created_at = Moment(chatroom[index].created_at).format('YYYY-MM-DD HH:mm:ss')
+      chatroom[index].modified_at = Moment(chatroom[index].modified_at).format('YYYY-MM-DD HH:mm:ss')
+    }
+    socket.emit('chatroom', chatroom)
+
   } catch (error) {
     console.log(error);
   }
