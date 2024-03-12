4f98de35034f51c481e19e94e4eb741bac5a4239
liaojiangkun@kanzhun.com <liaojiangkun@kanzhun.com>
2024-03-12 18:05:54 +0800

    30 日 echarts 图

--- build/app/Controllers/Http/admin/DataController.js
+++ build/app/Controllers/Http/admin/DataController.js
@@ -10,7 +10,7 @@ class DataController {
     async index({ request, response, view, session }) {
         try {
             const all = request.all(), datas = await Database_1.default.table('datas');
-            return view.render('admin/datAS/index', {
+            return view.render('admin/datas/index', {
                 data: {
                     title: '数据',
                     active: 'datas',
@@ -35,9 +35,11 @@ class DataController {
             UNION ALL
             SELECT 'users_type_2' AS type, count(*) AS value, '普通用户' AS text, '人' AS unit FROM users WHERE type = 2
             UNION ALL
-            SELECT 'users_current_today' AS type, count(*) AS value, '今日在线' AS text, '人' AS unit FROM users WHERE DATE(online_at) = CURDATE()
-            UNION ALL
             SELECT 'users_add_today' AS type, count(*) AS value, '新增用户' AS text, '人' AS unit FROM users WHERE DATE(created_at) = CURDATE()
+            UNION ALL
+            SELECT 'users_current_today' AS type, count(*) AS value, '日活跃用户' AS text, '人' AS unit FROM users WHERE DATE(online_at) = CURDATE()
+            UNION ALL
+            SELECT 'users_current_month' AS type, count(*) AS value, '月活跃用户' AS text, '人' AS unit FROM users WHERE MONTH(online_at) = MONTH(CURDATE()) AND YEAR(online_at) = YEAR(CURDATE())
           ) AS data
         `))[0],
                 customer: (await Database_1.default.rawQuery(`
@@ -64,14 +66,57 @@ class DataController {
             SELECT 'moment' AS type, count(*) AS value, '动态' AS text, '条' AS unit FROM moments WHERE status = 1
           ) AS data
         `))[0],
+                charts: {
+                    total: (await Database_1.default.rawQuery(`
+            SELECT
+                date_range.day,
+                COALESCE(count_users, 0) AS count_users,
+                COALESCE(count_customer, 0) AS count_customer,
+                COALESCE(count_moments, 0) AS count_moments
+            FROM (
+                SELECT DATE_SUB(CURDATE(), INTERVAL seq DAY) AS day,
+                      COUNT(users.created_at) AS count_users,
+                      COUNT(customer.created_at) AS count_customer,
+                      COUNT(moments.created_at) AS count_moments
+                FROM (
+                    SELECT 0 AS seq UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL 
+                    SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL 
+                    SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9 UNION ALL 
+                    SELECT 10 UNION ALL SELECT 11 UNION ALL SELECT 12 UNION ALL 
+                    SELECT 13 UNION ALL SELECT 14 UNION ALL SELECT 15 UNION ALL 
+                    SELECT 16 UNION ALL SELECT 17 UNION ALL SELECT 18 UNION ALL 
+                    SELECT 19 UNION ALL SELECT 20 UNION ALL SELECT 21 UNION ALL 
+                    SELECT 22 UNION ALL SELECT 23 UNION ALL SELECT 24 UNION ALL 
+                    SELECT 25 UNION ALL SELECT 26 UNION ALL SELECT 27 UNION ALL SELECT 28 UNION ALL SELECT 29
+                ) AS dates 
+                LEFT JOIN users ON DATE(users.created_at) = DATE_SUB(CURDATE(), INTERVAL seq DAY) 
+                LEFT JOIN customer ON DATE(customer.created_at) = DATE_SUB(CURDATE(), INTERVAL seq DAY) 
+                LEFT JOIN moments ON DATE(moments.created_at) = DATE_SUB(CURDATE(), INTERVAL seq DAY)
+                GROUP BY day
+                ORDER BY day DESC
+            ) date_range
+            ORDER BY date_range.day;
+          `))[0],
+                    count: {
+                        day: [],
+                        users: [],
+                        customer: [],
+                        moments: [],
+                    }
+                }
             };
-            return view.render('admin/datAS/board', {
+            for (var i = 0; i < datas.charts.total.length; i++) {
+                datas.charts.count.day.push((0, moment_1.default)(datas.charts.total[i].day).format("YYYY-MM-DD"));
+                datas.charts.count.users.push(datas.charts.total[i].count_users);
+                datas.charts.count.customer.push(datas.charts.total[i].count_customer);
+                datas.charts.count.moments.push(datas.charts.total[i].count_moments);
+            }
+            return view.render('admin/datas/board', {
                 data: {
                     title: '平台数据',
                     active: 'datas',
                     subActive: 'board',
-                    datas,
-                    all
+                    datas
                 }
             });
         }
@@ -95,7 +140,7 @@ class DataController {
         ) AS data
       `))[0]
             };
-            return view.render('admin/datAS/portrait', {
+            return view.render('admin/datas/portrait', {
                 data: {
                     title: '画像洞察',
                     active: 'datas',
